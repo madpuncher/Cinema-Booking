ae409cd388affbba89d58a760e6d03a9644f3ba8
Eldar Tengizov <madpunch@mail.ru>
2021-05-05 12:05:56 +0300

    Initial Commit

---
+++ Cinema Booking/Cinema_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinema_BookingApp.swift
+//  Cinema Booking
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 05.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Cinema_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
